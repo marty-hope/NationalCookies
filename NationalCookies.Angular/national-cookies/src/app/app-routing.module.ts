5bc267dc0de6c4da4c0e311555934ca5ce723199
marty-hope <marty.hope@gmail.com>
2021-01-03 15:42:22 -0800

    Add routing Move nav to app component

--- NationalCookies.Angular/national-cookies/src/app/app-routing.module.ts
+++ NationalCookies.Angular/national-cookies/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+import { CookiesComponent } from './pages/cookies/cookies/cookies.component';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'cookies', component: CookiesComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: '**', redirectTo: 'main' }
+
+];
+
+const config: ExtraOptions = {
+  onSameUrlNavigation: 'reload'
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, config)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
